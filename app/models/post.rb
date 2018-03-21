3d8e61dd9c1ff35dc0014d957d75f9eeaf235dca
Rachel Miller <raeanne1991@gmail.com>
2018-03-21 15:16:43 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    if self.title == nil
+      errors.add(:title, "You must have a title")
+    elsif self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top") || self.title.include?("Guess")
+    else
+        errors.add(:title, "Title must be more clickbaity")
+    end
+  end
+
 end
