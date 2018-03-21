28c0a795099f4f95794abfed3a09b0a4124f826c
Rachel Miller <raeanne1991@gmail.com>
2018-03-21 14:47:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    if :title == "Won't Believe"  || :title == "Secret" || :title == "Top [number]" || :title ==  "Guess"
+      true
+    end
+  end
+
 end
